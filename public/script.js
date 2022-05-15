a34f83c0977cfbdd89a9a33b8fb6c8cfb7b95af3
prasantdev <prasantsahoorebel@gmail.com>
2022-05-15 18:09:09 +0530

    add space for blank commit

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ var mediaConstraints = {
   audio: true,
   video: {
     aspectRatio: {
-      ideal: 1.333333 // 3:2 aspect is preferred
+      ideal: 1.333333 // 3:2 aspect is preferred 
     }
   }
 };
